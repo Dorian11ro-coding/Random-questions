ef9555fc6614d0fed08bdd1a34b0a3c55c98e609
Dorian <minimickey427@yahoo.com>
2024-01-21 18:07:50 +0200

    Initial Commit

---
+++ Random questions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Random questions
+//
+//  Created by Scarlat  Dorian Andrei on 21.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
